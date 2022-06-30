d945eae3607b50a3e0aee499b8f00e8e28e2d49b
Mona Lisa <daint@gmail.com>
2022-06-30 17:28:46 +0700

    a

---
+++ scale.py
@@ -0,0 +1,30 @@
+# When your data has different values, and even different measurement units, 
+# it can be difficult to compare them.
+# What is kilograms compared to meters? Or altitude compared to time?
+# The answer to this problem is scaling. we can scale data into new values that are easier to compare.
+# Take a look at the table below. it is the same data set that we used in the multiple regression chapter.
+# but this time the volume column contains values in liters instead of cm3
+
+# It can be difficult to compare the volume 1.0 with the weight 790.
+# but if we scale them both into comparable values. we can easily see how much one value is compared to the other.
+# There are different methods for scaling data, we will use a method called standardization.
+# The standardization method uses this formula:
+# z = (x - u) /s
+# where 
+# z is the new value
+# x is original value
+# u is the mean 
+# s is the standard deviation
+import pandas
+from sklearn import linear_model
+from sklearn.preprocessing import StandardScaler
+
+scale = StandardScaler()
+
+df = pandas.read_csv("data/cars2.csv")
+
+x = df[["Weight", "Volume"]]
+
+scaleX = scale.fit_transform(x)
+
+print(scaleX)
