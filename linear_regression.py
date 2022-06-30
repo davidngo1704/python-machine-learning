f7dcd1f33cbe3c3e9159d92ad15a264c47ad46ab
Mona Lisa <daint@gmail.com>
2022-06-30 09:37:51 +0700

    a

---
+++ linear_regression.py
@@ -0,0 +1,24 @@
+# Regression
+# The term regression is used when you try to find the relationship between variables.
+# In machine learning, and in statistical modeling. that relationship is used to predict the outcome of future events.
+# Linear Regession uses the relationship between the data-points to draw a straight line through all them.
+# This line can be used to predict future values.
+# In machine learning, predicting the future is very impotant.
+
+import matplotlib.pyplot as plt
+from scipy import stats
+
+
+x = [5,7,8,7,2,17,2,9,4,11,12,9,6]
+y = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+
+slope, intercept, r, p, std_err = stats.linregress(x, y)
+
+def myfunc(x):
+    return slope * x + intercept
+
+mymodel = list(map(myfunc, x))
+print(slope, intercept)
+plt.scatter(x, y)
+plt.plot(x, mymodel)
+plt.show()
