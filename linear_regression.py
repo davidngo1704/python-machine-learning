74c7a05b9a06085cf828f45e40b72ef8eab1bf46
Mona Lisa <daint@gmail.com>
2022-06-30 10:03:29 +0700

    a

--- linear_regression.py
+++ linear_regression.py
@@ -13,7 +13,7 @@ x = [5,7,8,7,2,17,2,9,4,11,12,9,6]
 y = [99,86,87,88,111,86,103,87,94,78,77,85,86]
 
 slope, intercept, r, p, std_err = stats.linregress(x, y)
-
+print(r)
 def myfunc(x):
     return slope * x + intercept
 
@@ -21,3 +21,7 @@ mymodel = list(map(myfunc, x))
 plt.scatter(x, y)
 plt.plot(x, mymodel)
 plt.show()
+
+# R for relationship 
+# it is important to know the relationship between the values of the x-axis and the values of the y-axis
+# 
