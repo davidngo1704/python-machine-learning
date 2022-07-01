3ecc5c87bd6c7cf6f387cae634d5499695d971cc
Mona Lisa <daint@gmail.com>
2022-07-01 10:00:25 +0700

    a

---
+++ decision_tree.py
@@ -0,0 +1,4 @@
+# A Decision Tree is a flow chart. and can help you make decisions based on previous experience.
+# In the example, a persion will try to decide if he should go to a comedy show or not.
+# Luckily out example persion has registered every time there was a comedy there was a comedy show in town.
+# and registered some information about the comedian, and also registered if he went or not.
