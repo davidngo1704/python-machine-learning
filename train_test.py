b5ce6d9ae059c498837dee46fb7ffc5b6e2dc03a
Mona Lisa <daint@gmail.com>
2022-07-01 09:37:14 +0700

    a

--- train_test.py
+++ train_test.py
@@ -1,5 +1,6 @@
 import numpy
 import matplotlib.pyplot as plt
+from sklearn.metrics import r2_score
 numpy.random.seed(2)
 
 # The x axis represents the number of minutes before making a purchase.
@@ -15,8 +16,13 @@ test_y = y[80:]
 
 mymodel = numpy.poly1d(numpy.polyfit(train_x, train_y, 4))
 
-myline = numpy.linspace(0, 6, 100)
+r2 = r2_score(test_y, mymodel(test_x))
 
-plt.scatter(train_x ,train_y)
-plt.plot(myline, mymodel(myline))
-plt.show()
+print(r2)
+
+
+# myline = numpy.linspace(0, 6, 100)
+
+# plt.scatter(train_x ,train_y)
+# plt.plot(myline, mymodel(myline))
+# plt.show()
